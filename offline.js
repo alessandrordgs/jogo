bddee53147f81fa67ef245f4f7ea02bfb076ac80
alessandrordgs <ale.silva1120@gmail.com>
2021-11-21 21:25:57 -0300

    teste

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1637540648,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/personagem-sheet0.png",
+		"images/caixa-sheet0.png",
+		"images/tilemap.png",
+		"images/espada-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
